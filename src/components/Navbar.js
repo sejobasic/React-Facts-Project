08c1040eda70c700818c3d20b408d95424c5d66a
Sejo Basic <sejoob@gmail.com>
2023-01-16 13:17:36 -0700

    Navbar and Main component structure completed - added background image to body - project completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import '../App.css'
 import reactIcon from '../assets/reacticon.png'
+import menu from '../assets/menu.png'
 
 function Navbar() {
   return (
     <nav>
-      <img src={reactIcon} alt='react icon' />
-    <h3>ReactFacts</h3>
-  </nav>
+      <img className='nav-logo' src={reactIcon} alt='react icon' />
+      <h3>ReactFacts</h3>
+      <img className='nav-menu' src={menu} alt='menu icon' />
+    </nav>
   )
 }
 
